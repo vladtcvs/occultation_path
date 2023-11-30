4d3b7efbfecc7f180a88d908dc8a38aff954befe
Vladislav Tsendrovskii <vtcendrovskii@gmail.com>
2023-12-01 04:57:04 +0500

    Parse different cases of format

--- parser_tool.py
+++ parser_tool.py
@@ -1,6 +1,5 @@
 """Parse occultation descriptions from www.asteroidoccultation.com"""
 import sys
-import csv
 import datetime
 import geojson
 
@@ -10,6 +9,7 @@ class PathParser:
     def _select_path_area(lines):
         data_area = False
         data_lines = []
+        options = {}
         for line in lines:
             if line.startswith("Path Coordinates"):
                 data_area = True
@@ -17,128 +17,104 @@ class PathParser:
                 data_area = False
                 break
             if data_area:
-                line = line.strip()
-                if line != "":
+                if line.strip() != "":
                     data_lines.append(line)
-        data_lines = data_lines[6:]
-        return data_lines
-
-    @staticmethod
-    def _parse_line(line):
-        line = line.replace("-  ", "-00")
-        line = line.replace("- ", "-0")
-        items = line.split(" ")
-        items = [item for item in items if item != '']
-
-        plus_day = False
-        if items[9] == "+":
-            plus_day = True
-            del items[9]
-        fields = {
-            "lon_D" : items[0],
-            "lon_M" : items[1],
-            "lon_S" : items[2],
-
-            "center_lat_D" : items[3],
-            "center_lat_M" : items[4],
-            "center_lat_S" : items[5],
-
-            "plus_day" : plus_day,
-            "ut_H" : items[6],
-            "ut_M" : items[7],
-            "ut_S" : items[8],
-
-            "star_ALT" : items[9],
-            "star_AZ"  : items[10],
-
-            "sun_ALT"  : items[11],
-
-            "path_limit_1_lat_D" : items[12],
-            "path_limit_1_lat_M" : items[13],
-            "path_limit_1_lat_S" : items[14],
-
-            "path_limit_2_lat_D" : items[15],
-            "path_limit_2_lat_M" : items[16],
-            "path_limit_2_lat_S" : items[17],
-
-            "err_limit_3_lat_D" : items[18],
-            "err_limit_3_lat_M" : items[19],
-            "err_limit_3_lat_S" : items[20],
-
-            "err_limit_4_lat_D" : items[21],
-            "err_limit_4_lat_M" : items[22],
-            "err_limit_4_lat_S" : items[23],
-        }
-        return fields
-
-    @staticmethod
-    def _transform_DMS(D, M, S):
-        if "-" in D:
-            sign = -1
+        if "Longitude" in data_lines[5]:
+            options["format_lon"] = True
         else:
-            sign = 1
-
-        D = abs(int(D))
-        M = int(M)
-        S = float(S)
-        return sign * (D + M/60 + S/3600)
+            options["format_lon"] = False
+        data_lines = data_lines[6:]
+        return data_lines, options
 
     @staticmethod
-    def _transform_timestamp(date : str, H : str, M : str, S : str, plus_day : bool):
-        ts = datetime.datetime.strptime(date, "%Y %b %d")
-        if plus_day:
-            ts += datetime.timedelta(days=1)
-        h = int(H)
-        m = int(M)
-        s = float(S)
+    def parse_lon(line : str):
+        sign = 1
+        if line[0] == '-':
+            sign = -1
+        d = int(line[1:4])
+        m = int(line[5:7])
+        s = int(line[8:10])
+        lon = sign * (d + m / 60 + s / 3600)
+        return lon, line[10:]
+
+    @staticmethod
+    def parse_lat(line : str):
+        sign = 1
+        if line[0] == '-':
+            sign = -1
+        d = int(line[1:3])
+        m = int(line[4:6])
+        s = int(line[7:9])
+        lat = sign * (d + m / 60 + s / 3600)
+        return lat, line[9:]
+
+    @staticmethod
+    def parse_hms(line : str, date : datetime.datetime):
+        h = int(line[0:2])
+        m = int(line[3:5])
+        s = float(line[6:10])
+        pd = (line[11] == '+')
+        if pd:
+            date = date + datetime.timedelta(days=1)
         us = int((s - int(s))*1000000)
         s = int(s)
-        ts = datetime.datetime(ts.year, ts.month, ts.day, h, m, s, us)
+        ts = datetime.datetime(date.year, date.month, date.day, h, m, s, us)
         ts = ts.isoformat("T")
-        return ts
+        return ts, line[12:]
 
     @staticmethod
-    def _transform_fields(fields : dict, date : str):
-        try:
-            transform_DMS = PathParser._transform_DMS
-            transformed = {
-                "lon" : transform_DMS(fields["lon_D"],
-                                      fields["lon_M"],
-                                      fields["lon_S"]),
+    def parse_int(line : str, digits : int):
+        return int(line[:digits]), line[digits:]
 
-                "center_lat" : transform_DMS(fields["center_lat_D"],
-                                             fields["center_lat_M"],
-                                             fields["center_lat_S"]),
+    @staticmethod
+    def parse_float(line : str, digits : int):
+        return float(line[:digits]), line[digits:]
 
-                "limit1_lat" : transform_DMS(fields["path_limit_1_lat_D"],
-                                             fields["path_limit_1_lat_M"],
-                                             fields["path_limit_1_lat_S"]),
+    @staticmethod
+    def _parse_line(line : str, date : datetime.datetime, format_lon : bool):
+        line = line[3:]
+        fields = {}
+        fields["center_lon"], line = PathParser.parse_lon(line)
+        line = line[3:]
+        fields["center_lat"], line = PathParser.parse_lat(line)
+        line = line[3:]
+        fields["UT"], line = PathParser.parse_hms(line, date)
+        line = line[2:]
+        fields["star_alt"], line = PathParser.parse_int(line, 2)
+        line = line[3:]
+        fields["star_az"], line = PathParser.parse_int(line, 3)
+        line = line[3:]
+        fields["sun_alt"], line = PathParser.parse_int(line, 3)
 
-                "limit2_lat" : transform_DMS(fields["path_limit_2_lat_D"],
-                                             fields["path_limit_2_lat_M"],
-                                             fields["path_limit_2_lat_S"]),
-
-                "limit3_lat" : transform_DMS(fields["err_limit_3_lat_D"],
-                                             fields["err_limit_3_lat_M"],
-                                             fields["err_limit_3_lat_S"]),
-
-                "limit4_lat" : transform_DMS(fields["err_limit_4_lat_D"],
-                                             fields["err_limit_4_lat_M"],
-                                             fields["err_limit_4_lat_S"]),
-
-                "UT" : PathParser._transform_timestamp(date,
-                                                       fields["ut_H"],
-                                                       fields["ut_M"],
-                                                       fields["ut_S"],
-                                                       fields["plus_day"]),
-
-                "sun_alt" : int(fields["sun_ALT"]),
-                "star_alt" : int(fields["star_ALT"]),
-                "star_az" : int(fields["star_AZ"]),
-            }
-            return transformed, ""
-        except ValueError as e:
-            return None, str(e)
+        if format_lon:
+            line = line[1:]
+            fields["path_limit1_lon"], line = PathParser.parse_lon(line)
+            line = line[2:]
+            fields["path_limit1_lat"], line = PathParser.parse_lat(line)
+            line = line[1:]
+            fields["path_limit2_lon"], line = PathParser.parse_lon(line)
+            line = line[2:]
+            fields["path_limit2_lat"], line = PathParser.parse_lat(line)
+            line = line[1:]
+            fields["err_limit1_lon"], line = PathParser.parse_lon(line)
+            line = line[2:]
+            fields["err_limit1_lat"], line = PathParser.parse_lat(line)
+            line = line[1:]
+            fields["err_limit2_lon"], line = PathParser.parse_lon(line)
+            line = line[2:]
+            fields["err_limit2_lat"], line = PathParser.parse_lat(line)
+        else:
+            line = line[2:]
+            fields["path_limit1_lat"], line = PathParser.parse_lat(line)
+            line = line[2:]
+            fields["path_limit2_lat"], line = PathParser.parse_lat(line)
+            line = line[2:]
+            fields["err_limit1_lat"], line = PathParser.parse_lat(line)
+            line = line[2:]
+            fields["err_limit2_lat"], line = PathParser.parse_lat(line)
+        line = line[2:]
+        fields["alt_crn"], line = PathParser.parse_float(line, 5)
+        return fields
 
     @staticmethod
     def _parse_event(line : str):
@@ -159,51 +135,22 @@ class PathParser:
         """Parse occultation prediction"""
         lines = text.splitlines()
         event = PathParser._parse_event(lines[1])
-        data_lines = PathParser._select_path_area(lines)
+        data_lines, data_options = PathParser._select_path_area(lines)
         timings = []
+        ts = datetime.datetime.strptime(event["date"], "%Y %b %d")
         for index, line in enumerate(data_lines):
-            line_items = PathParser._parse_line(line)
-            fields, err = PathParser._transform_fields(line_items, event["date"])
-            if fields is not None:
-                timings.append(fields)
-            else:
-                print(f"Point #{index} : {err}")
+            try:
+                fields = PathParser._parse_line(line, ts, data_options["format_lon"])
+            except Exception as e:
+                print("Error %s on line #%i skip" % (e, index))
+                continue
+            timings.append(fields)
+
         result = {
             "event" : event,
             "timings" : timings,
         }
         return result
-
-def process_file_csv(input_fname, output_fname):
-    """Convert to csv"""
-    with open(input_fname, encoding='utf8') as f:
-        text = f.read()
-
-    psr = PathParser()
-    points = psr.parse_data(text)["timings"]
-    with open(output_fname, "w", encoding='utf8') as f:
-        writer = csv.writer(f)
-        writer.writerow(["lon",
-                         "center_lat",
-                         "limit1_lat",
-                         "limit2_lat",
-                         "limit3_lat",
-                         "limit4_lat",
-                         "sun_alt",
-                         "star_alt",
-                         "star_az",
-                         "UT"])
-        for fields in points:
-            writer.writerow([fields["lon"],
-                             fields["center_lat"],
-                             fields["limit1_lat"],
-                             fields["limit2_lat"],
-                             fields["limit3_lat"],
-                             fields["limit4_lat"],
-                             fields["sun_alt"],
-                             fields["star_alt"],
-                             fields["star_az"],
-                             fields["UT"]])
 
 def process_file_geojson(input_fname, output_fname):
     """Convert to geojson"""
@@ -226,14 +173,18 @@ def process_file_geojson(input_fname, output_fname):
                 "star" : event["star"],
                 "planet" : event["planet"],
             }
-            lon = fields["lon"]
+            lon = fields["center_lon"]
             lat = fields["center_lat"]
             features.append(geojson.Feature(geometry=geojson.Point((lon, lat)),
                                             properties=props))
-            lat = fields["limit1_lat"]
+            if "path_limit1_lon" in fields:
+                lon = fields["path_limit1_lon"]
+            lat = fields["path_limit1_lat"]
             features.append(geojson.Feature(geometry=geojson.Point((lon, lat)),
                                             properties=props))
-            lat = fields["limit2_lat"]
+            if "path_limit2_lon" in fields:
+                lon = fields["path_limit2_lon"]
+            lat = fields["path_limit2_lat"]
             features.append(geojson.Feature(geometry=geojson.Point((lon, lat)),
                                             properties=props))
 
